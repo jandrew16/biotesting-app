02cfb69f709c9ae5b67100e185427e1b82219ad4
Your Name <you@example.com>
2021-11-29 07:47:12 -0700

    first commit

---
+++ src/modules/comps/customButton/customButton.js
@@ -0,0 +1,12 @@
+import { LightningElement, api } from "lwc";
+
+export default class CustomButton extends LightningElement {
+
+    @api
+    CustomButton;
+
+
+}
+carting ()
+    let numclicks = TimesClicked
+    
---
+++ src/modules/comps/header/head.js
@@ -0,0 +1,5 @@
+import { LightningElement } from 'lwc';
+
+export default class Head extends LightningElement {
+    
+}
